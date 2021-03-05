e29024c417764a611383abf9d7445e745020573c
Daniel <daniel.roa98@gmail.com>
2021-03-05 15:20:02 -0600

    Updated main.py code

--- Computational intelligence/HomeAssignment1/main.py
+++ Computational intelligence/HomeAssignment1/main.py
@@ -1,14 +1,41 @@
-import pandas as pd
+'''
+Code meant to solve the first home assignment, specifically the first excercise.
 
+Daniel Roa - A01021960
+05/03/2021
+'''
+#Imported libraries
+import pandas as pd
+import random
+from datetime import datetime
+
+#conn.log file is located in the resource folder
 logFile = './resources/conn.log'
 
-convLog = pd.read_csv(logFile, sep='\t', header=None, names=[
-    'ts', 'uid', 'id_orig_h', 'id_orig_p', 'id_resp_h', 'id.resp_p', 'proto', 'service', 
+#Size of the sample will be the equivalent to the 10% of the total sample.
+sampleSize = 0.1 
+
+#Reading the conn.log file and using the total amount of the sample as well
+convLog = pd.read_csv(logFile, sep='\t', header=None, skiprows=lambda i: i > 0 and random.random() > sampleSize, names=[
+    'ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'service', 
     'duration', 'orig_bytes', 'resp_bytes', 'conn_state', 'local_orig', 'missed_bytes', 
     'history', 'orig_pkts', 'orig_ip_bytes', 'resp_pkts', 'resp_ip_bytes', 'tunnel_parents', 
     'threat', 'sample'
 ])
 
-print(convLog.head())
+""" #Used to check the overall size of the sample
+print('Sample information\n', convLog.info())
+#Used to check the sample's column heads.
+print('Sample heads\n', convLog.head()) """
 
-convLog.DataFrame.sample(n=None, frac=0.1, replace=False, weights=None, random_state=None, axis=None)
+#Convert the timestamp to datetime and sort the sample
+convLog['ts'] = [
+    datetime.fromtimestamp(float(date))
+    for date in convLog['ts'].values
+]
+
+convLog = convLog.set_index('ts')
+convLog.index = pd.to_datetime(convLog.index)
+convLog = convLog.sort_index()
+
+print(convLog)
